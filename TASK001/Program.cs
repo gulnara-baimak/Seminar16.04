f791fc89a765199463cc90e42274347e850f4525
Gulnara <gulnara@mail.rum>
2022-04-17 00:01:11 +0600

    на вход принимает два числа и выдает max, min

---
+++ TASK001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. 
+
+int a = -9;
+int b = -3;
+Console.WriteLine(a);
+Console.WriteLine(b);
+if  
+    (a > b)
+        Console.WriteLine("max a");
+else
+        Console.WriteLine("max b");
+return;
